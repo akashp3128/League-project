18a46c76734c3b9131c0ad78a6a7ce02134f696b
apatel1 <akashp3128@gmail.com>
2023-10-31 18:58:17 -0500

    more updates[C

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,12 +8,13 @@ const Home = () => {
         <div className="col-md-8">
           <div className="container custom-text-color">
             <h1 className="text-center mt-4">Welcome to Sleeper Data App</h1>
+            <p>Power rankings chart here</p>
           </div>
         </div>
         <div className="col-md-4">
           <div className="container custom-text-color">
-            <h1 className="text-center mt-4">Right Side Container</h1>
-            <p>Content for the right side container goes here.</p>
+            <h1 className="text-center mt-4">Week current.week matchups</h1>
+            <p>weekly mathchups info here, create stack?</p>
           </div>
         </div>
       </div>
