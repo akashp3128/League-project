009dbdc07a8921c0639038b5108b218365f10cee
apatel1 <akashp3128@gmail.com>
2023-10-31 18:55:00 -0500

    added right side container to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,19 @@ import './styles.css'; // You can keep your custom styles here
 const Home = () => {
   return (
     <div className="container">
-      <h1 className="text-center mt-4 custom-text-color">Welcome to Sleeper Data App</h1>
+      <div className="row">
+        <div className="col-md-8">
+          <div className="container custom-text-color">
+            <h1 className="text-center mt-4">Welcome to Sleeper Data App</h1>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="container custom-text-color">
+            <h1 className="text-center mt-4">Right Side Container</h1>
+            <p>Content for the right side container goes here.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
