41aac8423c892b2ee542208441b40212b11c35ee
apatel1 <akashp3128@gmail.com>
2023-11-07 19:54:37 -0600

    added card fucntionality for roster

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -4,10 +4,41 @@ import { getRostersInLeague } from '../services/getRoster';
 import { getTeam } from '../services/getTeam';
 import { getAllPlayers } from '../services/getPlayers';
 
+function TeamCard({ teamEntry, players }) {
+    const [isExpanded, setIsExpanded] = useState(false);
+  
+    const toggleExpansion = () => {
+      setIsExpanded(!isExpanded);
+    };
+  
+    return (
+      <div className={`team-card ${isExpanded ? 'expanded' : ''}`} onClick={toggleExpansion}>
+        <h3 className='color-sleeperblue'>{teamEntry && teamEntry.team ? teamEntry.team.name : 'Unknown Team'}</h3>
+        {isExpanded && (
+          <div className="roster">
+            <h4 className='color-sleeperblue'>Roster:</h4>
+            <ul className='color-sleeperblue'>
+              {teamEntry.roster.players.map((playerId) => {
+                const playerData = players[playerId];
+                return (
+                  <li key={playerId}>
+                    {playerData ? (playerData.search_first_name && playerData.search_last_name ? `${playerData.search_first_name} ${playerData.search_last_name}` : 'Unknown Player') : 'Unknown Player'}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+    );
+  }
+  
+
 function Test() {
   const [leagueInfo, setLeagueInfo] = useState(null);
   const [rosters, setRosters] = useState([]);
   const [players, setPlayers] = useState([]);
+  const [teamData, setTeamData] = useState([]);
   const leagueId = '983853389404590080';
 
   useEffect(() => {
@@ -49,8 +80,6 @@ function Test() {
     fetchAllPlayers();
   }, []);
 
-  const [teamData, setTeamData] = useState([]);
-
   useEffect(() => {
     const fetchTeamData = async () => {
       const teams = rosters.map(async (roster) => {
@@ -71,24 +100,9 @@ function Test() {
 
   return (
     <div>
-
       <h2 className='color-white'>Teams and Rosters</h2>
       {teamData.map((teamEntry, index) => (
-        <div key={index}>
-          <h3 className='color-white'>{teamEntry && teamEntry.team ? teamEntry.team.name : 'Unknown Team'}</h3>
-          <h4 className='color-white'>Roster:</h4>
-          <ul className='color-white'>
-            {teamEntry.roster.players.map((playerId) => {
-              // Find the player data for the current player ID
-              const playerData = players[playerId];
-              return (
-                <li key={playerId}>
-                 {playerData ? (playerData.search_first_name && playerData.search_last_name ? `${playerData.search_first_name} ${playerData.search_last_name}` : 'Unknown Player') : 'Unknown Player'}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        <TeamCard key={index} teamEntry={teamEntry} players={players} />
       ))}
     </div>
   );
